837dd571b2d668590c5754c6d76fc5c54c0c42c0
Michal Bachman <michal@graphaware.com>
2015-10-18 18:03:51 +0200

    split modules

---
+++ src/test/java/com/graphaware/integration/es/util/TestUtil.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2015 GraphAware
+ *
+ * This file is part of GraphAware.
+ *
+ * GraphAware is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.graphaware.integration.es.util;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by bachmanm on 18/10/2015.
+ */
+public class TestUtil {
+
+    private TestUtil() {
+    }
+
+    public static void waitFor(long ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
